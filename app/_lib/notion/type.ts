d5e895b38085039d1ab64a7b692fd4e3421a97aa
takumines <takumines1@gmail.com>
2023-12-29 21:25:04 +0900

    [add] notion clientの型を使いやすいように拡張するtypeファイルを作成

---
+++ app/_lib/notion/type.ts
@@ -0,0 +1,15 @@
+import { PageObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+
+type ExtractMultiSelectType<T> = T extends {
+  multi_select: infer U
+  type: 'multi_select'
+}
+  ? U
+  : never
+
+/**
+ * PageObjectResponseのプロパティの型からMultiSelectの型を抽出する
+ */
+export type MultiSelectType = ExtractMultiSelectType<
+  PageObjectResponse['properties'][keyof PageObjectResponse['properties']]
+>
