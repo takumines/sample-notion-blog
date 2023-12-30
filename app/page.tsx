985cb06ee464726a1b46c2af8982d9f1d229186b
takumines <takumines1@gmail.com>
2023-12-30 10:41:36 +0900

    [update] 記事一覧画面の修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { PostListCard } from '@/app/_components/post'
 import { getAllPosts } from '@/app/_lib/notion/client'
 import Head from 'next/head'
 
@@ -5,13 +6,20 @@ export const revalidate = 3600
 
 export default async function Home() {
   const allPosts = await getAllPosts()
-  console.log(allPosts)
+
   return (
-    <div>
+    <div className="container mx-auto h-full w-full font-mono">
       <Head>
-        <title> All Posts</title>
+        <title>Notion Blog</title>
       </Head>
-      <p> All Posts</p>
+      <main className="container mt-16 w-full">
+        <h1 className="mb-16 text-center text-5xl font-medium">
+          Notion Blog🚀
+        </h1>
+        {allPosts.map((post) => (
+          <PostListCard key={post.id} post={post} />
+        ))}
+      </main>
     </div>
   )
 }
