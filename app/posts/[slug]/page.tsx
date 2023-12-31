5faa36421e2e24af5ee80d2458f3d79b06963563
takumines <takumines1@gmail.com>
2023-12-31 12:56:03 +0900

    [add] 記事詳細画面の作成

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { getAllPostList, getPostBySlug } from '@/app/_lib/notion'
+import { notFound } from 'next/navigation'
+
+const Post = async ({ params }: { params: { slug: string } }) => {
+  const post = await getPostBySlug(params.slug)
+
+  //  slugに紐づく記事が存在しない場合は、404ページを表示
+  if (!post) {
+    notFound()
+  }
+
+  const { date, description, tags, title } = post
+
+  return (
+    <section className="container mx-auto mt-20 h-screen px-5 lg:w-2/5 lg:px-2">
+      <h2 className="w-full text-2xl font-medium">{title}</h2>
+      <div className="mt-1 w-1/3 border-b-2 border-sky-200"></div>
+      <span className="text-gray-500">{date}</span>
+      <br />
+      {tags.map((tag) => (
+        <p
+          className="mr-2 mt-2 inline-block rounded-xl bg-sky-900 px-2 font-medium text-white"
+          key={tag}
+        >
+          {tag}
+        </p>
+      ))}
+      <div className="mt-10 font-medium">{description}</div>
+    </section>
+  )
+}
+
+export const generateStaticParams = async () => {
+  const postList = await getAllPostList()
+
+  return postList.map((post) => ({
+    slug: post.slug,
+  }))
+}
+
+export default Post
